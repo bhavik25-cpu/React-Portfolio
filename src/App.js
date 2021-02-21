187bd0fedf17000579faa2b6c106017a1f121c65
Arijit123Muk <ben.arijit@hotmail.com>
2021-02-21 22:17:54 +0530

    404 updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-
 import Home from './screens/Home'
 import Contact from './screens/Contact'
 import Project from './screens/Project'
 import Resume from './screens/Resume'
 import Navbar from './shared/Navbar';
 import Footer from './shared/Footer';
+import NotFound from './screens/NotFound';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
@@ -24,6 +24,9 @@ function App() {
           </Route>
           <Route exact path="/resume">
             <Resume />
+          </Route>
+          <Route exact path="*">
+            <NotFound />
           </Route>
         </Switch>
         <Footer />
--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,12 +1,12 @@
 import firebase from 'firebase';
 
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyBm_aBgUxA_SelY23MiUY5UtpzYP-phu_g",
+  authDomain: "arijit-mukherjee-2000.firebaseapp.com",
+  projectId: "arijit-mukherjee-2000",
+  storageBucket: "arijit-mukherjee-2000.appspot.com",
+  messagingSenderId: "705514956605",
+  appId: "1:705514956605:web:e04b273a85ba38b90d8754"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
